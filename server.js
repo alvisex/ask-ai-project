c73898be710c4d86c9b31ce87448cbeb573ff4a2
Alvisex <alvise.ld@gmail.com>
2024-08-23 13:18:22 -0600

    add server config and openai controller

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
-const envConfig = require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+import express from 'express'
+import cors from 'cors'
+import pkg from 'body-parser';
+const { json } = pkg;
 
 const port = 3000
 const app = express()
 app.use(cors())
-app.use(bodyParser.json())
+app.use(json())
+
+
+/* OpenAI stuff */
+import { OpenAI } from 'openai'
+/* const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+}) */
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
+// OpenAI chat completion
+app.post('/chat', async (req, res) => {
+  const messages = req.body.messages
+  console.log(req.body)
+  console.log(messages)
+  try {
+    if (messages == null) {
+      throw new Error('We have a problem - no prompt was provided')
+    }
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-4o-mini',
+      messages
+    })
+    const completion = response.data.choices[0].message
+    console.dir(response.data.choices[0])
+    return res.status(200).json({
+      success: true,
+      message: completion
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
