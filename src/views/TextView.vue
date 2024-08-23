d3703f43b14067d371632babd40d620e371f5e27
Alvisex <alvise.ld@gmail.com>
2024-08-23 14:50:14 -0600

    setup textchat store and UI

--- src/views/TextView.vue
+++ src/views/TextView.vue
@@ -1,3 +1,45 @@
-<script setup></script>
+<script setup>
+import { useTextChatStore } from '@/stores/textChat'
 
-<template></template>
+const textChatStore = useTextChatStore()
+
+function sendQuestion() {
+  textChatStore.createPrompt()
+  textChatStore.sendPrompt()
+}
+</script>
+
+<template>
+  <div class="container">
+    <textarea
+      rows="20"
+      v-model="textChatStore.text"
+      class="block w-full rounded-md border-0 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:p-1.5 sm:text-sm sm:leading-6 text-sm my-4"
+    />
+
+    <input v-model="textChatStore.question" placeholder="Send a message" class="question-input" />
+
+    <button
+      @click="sendQuestion()"
+      type="button"
+      class="chat-button button group relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 text-sm font-semibold bg-[#1a1a21] text-green-500 ring-1 ring-inset ring-gray-300"
+    >
+      Submit
+    </button>
+
+    <div
+      v-if="textChatStore.gptResponse.length > 0"
+      class="block w-full rounded-md border-0 bg-green-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:p-4 sm:text-sm sm:leading-6 text-sm my-4"
+    >
+      {{ textChatStore.gptResponse }}
+    </div>
+
+    <button
+      v-show="textChatStore.text"
+      @click="textChatStore.clearChat()"
+      class="button button-secondary"
+    >
+      Clear
+    </button>
+  </div>
+</template>
